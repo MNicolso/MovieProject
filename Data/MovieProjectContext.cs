11149373b4a8570fbd95d565ed187aea672c1421
MNicolso <71354582+MNicolso@users.noreply.github.com>
2020-10-14 16:15:11 -0700

    Add project files.

---
+++ Data/MovieProjectContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MovieProject.Models;
+
+namespace MovieProject.Data
+{
+    public class MovieProjectContext : DbContext
+    {
+        public MovieProjectContext(DbContextOptions<MovieProjectContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
